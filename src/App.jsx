72ed4186bdff770fb33de2daf2853324f7ea1ed6
Naman <pandyanaman2@gmail.com>
2025-03-12 10:17:41 +0530

    added separate repo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar.jsx'
+import Player from './components/player.jsx'
+import Display from './components/Display.jsx'
+import { PlayerContext } from './context/PlayerContext.jsx'
+
+const App = () => {
+  const {audioRef,track,songsData}=useContext(PlayerContext)
+  return (
+    <div className='h-screen bg-black'>
+      {
+        songsData.lenght !==0
+        ? 
+        <>
+        <div className='h-[90%] flex'>
+          <Sidebar/>
+          <Display/> 
+        </div>
+        <Player/>
+        </>
+        :null 
+      }
+      <audio ref={audioRef} src={track? track.file : ""} preload='auto'></audio>
+    </div>
+  )
+}
+
+export default App
